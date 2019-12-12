56b7223a711f69e5a0da3ff5e008f1991afea639
Ben Weinberg <weinbergben33@gmail.com>
2019-12-12 16:45:16 -0500

    betterh erokyu

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import socket from "socket.io";
+import credentials from "./credentials";
+import https from "https";
+import socketServices from "./socketServices";
+
+const app = express();
+let port;
+let server = https.createServer(credentials, app);
+port = 443;
+
+app.use(express.static("public"));
+
+const io = socket(server);
+
+io.on("connection", socketServices(io).listen);
+
+server.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
